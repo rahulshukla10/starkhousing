d759bb6771da0ab56209eb668efc3f7731d9031f
Rahul Shukla <rsrahulshukla365@gmail.com>
2020-09-25 16:59:28 +0530

    Completed 3 Components Create i.e property-card,property-list,nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property-card/property-card.component';
+import { PropertyListComponent } from './property-list/property-list.component';
+import { NarBarComponent } from './nar-bar/nar-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    NarBarComponent
   ],
   imports: [
     BrowserModule,
