9d6fd8f841ed8eee826946a49a1805294bbbe146
Rahul Shukla <rsrahulshukla365@gmail.com>
2020-09-25 20:00:03 +0530

    Till CSS animation is done

--- src/app/property-list/property-list.component.ts
+++ src/app/property-list/property-list.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties :Array<any> = [{
+    "Id" : 1,
+    "Name" : "Raul Ville",
+    "Type" : "House",
+    "Price" : 20000,
+  },
+  {
+    "Id" : 2,
+    "Name" : "Stark Ville",
+    "Type" : "House",
+    "Price" : 100,
+  },
+  {
+    "Id" : 3,
+    "Name" : "Stark erotic",
+    "Type" : "House",
+    "Price" : 10000,
+  },
+  {
+    "Id" : 4,
+    "Name" : "RaulStark",
+    "Type" : "House",
+    "Price" : 1600,
+  },
+  {
+    "Id" : 5,
+    "Name" : "Rahul ville",
+    "Type" : "House",
+    "Price" : 1000,
+  },
+  {
+    "Id" : 6,
+    "Name" : "Raul Stark",
+    "Type" : "House",
+    "Price" : 16000,
+  }
+]
+
   constructor() { }
 
   ngOnInit(): void {
